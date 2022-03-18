eb22839aabde47581db409fce139292abf785a88
0xVichy#1234 <Its_Vichy@protonmail.com>
2022-03-18 03:17:09 +0100

    🍪 Add title counter: generated / verified

--- lib/data.py
+++ lib/data.py
@@ -1,9 +1,20 @@
-import json, itertools 
+import json, itertools, threading, time, os
 from lib import mail
 
 __config__ = json.load(open('./config.json', 'r+'))
 
 class Data:
     def __init__(self) -> None:
+        self.generated = 0
+        self.verified  = 0
+
         self.email = mail.Gmail(__config__['mail'], __config__['password'])
         self.usernames = itertools.cycle(open('./data/usernames.txt', 'r+', encoding= 'utf-8', errors= 'ignore').read().splitlines())
+
+        threading.Thread(target= self.update_title_thread).start()
+
+    def update_title_thread(self) -> None:
+        if os.name == 'nt':
+            while True:
+                os.system(f'title GuildeadGen - github.com/its-vichy ^| Generated: {self.generated} - Verified: {self.verified}')
+                time.sleep(1)
