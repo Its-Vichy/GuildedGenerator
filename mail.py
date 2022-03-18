1836565b5342effff26d6b90bbe0fdf25d6d3465
0xVichy#1234 <Its_Vichy@protonmail.com>
2022-03-18 03:51:24 +0100

    🍫 Handle ban errors

---
+++ mail.py
@@ -0,0 +1,34 @@
+import random, string, threading, time
+from lib.console import Console
+from imap_tools import MailBox
+
+class Gmail:
+    def __init__(self, mail: str, password: str) -> None:
+        self.mail = mail
+        self.password = password
+        self.blacklist = []
+        self.mail_list = {}
+
+        threading.Thread(target= self.parser_thread).start()
+    
+    def parser_thread(self) -> None:
+        try:
+            with MailBox('imap.gmail.com', '993').login(self.mail, self.password, 'INBOX') as mailbox:
+                for msg in mailbox.fetch():
+                    try:
+                        body = msg.html
+                        to = str(msg.to[0]).lower()
+
+                        if to not in self.blacklist:
+                            verif = body.split('https://www.guilded.gg/api/email/verify?token=')[1].split('"')[0]
+                            self.mail_list[to] = verif
+                            self.blacklist.append(to)
+                    except Exception as e:
+                        Console.debug(f'[-] Mail error: {e}')
+            time.sleep(1)
+        except Exception as e:
+            Console.debug(f'[-] Error when connecting to imap server: {e}')
+    
+    @staticmethod
+    def get_mail(mail_base: str) -> str:        
+        return mail_base + "+" + "".join([random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(10)]) + "@gmail.com"
